09b7f3c6087f7525a536091f83579f2aab343043
Jjanisruduks <janis.ruduks157@gmail.com>
2023-08-08 16:25:21 +0300

    Added favicon
    Added pdf download float button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,27 @@ import GitHubButton from 'react-github-btn'
 import Link from "next/link";
 import Image from 'next/image'
 import { FormDataProvider } from "@/components/FormDataContext";
-import { Button, Layout } from "antd";
+import { Layout, FloatButton } from "antd";
+import { DownloadOutlined } from '@ant-design/icons';
 
 export default function Home() {
   const { Header } = Layout;
   return (
     <main>
+      <FloatButton
+        icon={<DownloadOutlined />}
+        type="primary"
+        href="/pdf"
+        shape="square"
+        description="PDF"
+      />
       <FormDataProvider>
         <Layout>
           <Header className="bg-white text-blue-500 shadow-sm flex flex-row">
             <Link href="/" className="font-bold">
               <div className='md:flex-row md:flex flex flex-row'>
                 <div>
-                <Image width='64' height="64" alt='logo' src='/logo.png' />
+                  <Image width='64' height="64" alt='logo' src='/logo.png' />
                 </div>
                 CV Builder
               </div>
@@ -36,31 +44,26 @@ export default function Home() {
               </GitHubButton>
             </div>
           </Header>
-        <div className="container mx-auto md:p-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="md:overflow-y-auto md:h-screen">
-              <BasicForm />
-              <EducationForm />
-              <WorkForm />
-              <ProjectForm />
-              <SkillsForm />
-            </div>
-            <div>
-              <div className="md:p-2 scale-90 md:scale-100">
-                <PageCV />
-                <div className="m-3">
-                  <Button
-                    className='bg-blue-500'
-                    type='primary'
-                    href="/pdf"
-                  >
-                    Download PDF
-                  </Button>
+          <div className="container mx-auto md:p-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="md:overflow-y-auto md:h-screen">
+                <BasicForm />
+                <EducationForm />
+                <WorkForm />
+                <ProjectForm />
+                <SkillsForm />
+              </div>
+              <div>
+                <div className="md:p-2 scale-90 md:scale-100">
+                  <div className="h-screen">
+                    <PageCV />
+                  </div>
+                  <div className="m-3">
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
         </Layout>
       </FormDataProvider>
     </main>
